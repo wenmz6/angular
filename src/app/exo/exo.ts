0e5e29c7c51ced7bbcc002ac31893aa0713aaeb0
wenmz6 <wendie.mazy@gmail.com>
2025-07-22 09:36:44 +0200

    test

--- src/app/exo/exo.ts
+++ src/app/exo/exo.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './exo.html',
   styleUrl: './exo.css'
 })
 export class Exo {
+  maVariable: string = 'bonjour';
+  twoWay: string = 'bonsoir';
 
+  maMethode(): void {
+    this.maVariable = this.twoWay
+  }
 }
